bf58471434d7611121d95ceb634e7d87c45d6711
DroPZsec <94165782+DroPZsec@users.noreply.github.com>
2021-11-12 09:35:54 +0100

    Add files via upload

---
+++ Requirements.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "=================================="
+echo "This would install all you need"
+echo "To use this Tool without bugs."
+echo "=================================="
+echo
+echo
+echo
+sudo apt-get update
+echo
+echo
+echo
+echo "=================================="
+echo
+echo
+echo
+sudo apt-get upgrade
+echo
+echo
+echo
+echo "=================================="
+echo
+echo
+echo
+sudo apt-get install nmap
+echo
+echo
+echo
+echo "=================================="
+echo
+echo
+echo
+chmod +x LittleAutoMAP.sh
+echo
+echo
+echo
+echo "=================================="
+echo "   Now run ./LittleAutoMAP.sh"
+echo "=================================="
+echo
+echo
+echo
+echo
+echo
+echo "=================================="
+echo "       Coded by vDroPZz"
+echo "   #BLACKSnapbackCodingTEAM"
+echo "=================================="
+exit
+/bin/sh
